a007665f183ed639d5b486ae8a9af4d8c017c8a8
edaagunes <edagunes00@hotmail.com>
2023-10-23 23:37:21 +0300

    Codes have been optimized and refactoring

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,12 +1,17 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Food : MonoBehaviour
 {
     public BoxCollider2D gridArea;
+    private Snake snake;
+
+    private void Awake()
+    {
+        snake = FindObjectOfType<Snake>();
+    }
 
     private void Start()
     {
@@ -15,23 +20,37 @@ public class Food : MonoBehaviour
 
     private void RandomizePosition()
     {
-        //yiyecegin uretilecegi konumlar sinirlandirildi
         Bounds bounds = this.gridArea.bounds;
 
-        //grid'lerden tasma olmamasi icin degerler yuvarlandi
-        float x = Random.Range(bounds.min.x, bounds.max.x);
-        float y = Random.Range(bounds.min.y, bounds.max.y);
+        //Sinirlarin icinde rastgele bir konum secildi
+        //Grid'e hizalanmasi icin deger yuvarlandi
+        int x = Mathf.RoundToInt(Random.Range(bounds.min.x, bounds.max.x));
+        int y = Mathf.RoundToInt(Random.Range(bounds.min.y, bounds.max.y));
+
+        //Yiyecegin yilan uzerinde uretilmesi engellendi
+        while (snake.Occupies(x, y))
+        {
+            x++;
+
+            if (x > bounds.max.x)
+            {
+                x = Mathf.RoundToInt(bounds.min.x);
+                y++;
+
+                if (y > bounds.max.y)
+                {
+                    y = Mathf.RoundToInt(bounds.min.y);
+                }
+            }
+        }
 
         //yiyecegin konumunu bu koordinatlara atandi
-        transform.position = new Vector3(Mathf.Round(x),Mathf.Round(y), 0.0f);
+        transform.position = new Vector2(x, y);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //yiyecek ile snake carpistiginda yiyecek yeni bir konumda uretilecek 
-        if (other.tag=="Player")
-        {
-            RandomizePosition();
-        }
+        //yiyecek ile snake carpistiginda yiyecek yeni bir konumda uretildi
+        RandomizePosition();
     }
 }
--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,32 +1,24 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Snake : MonoBehaviour
 {
-    private Vector2 _direction;
-    private List<Transform> _segments;
     public Transform segmentPrefab;
+    private Vector2Int _direction = Vector2Int.right;
+    public float speed = 20f;
+    public float speedMultiplier = 1f;
+    public int initialSize = 4;
+    public bool moveThroughWalls = false;
 
-    private void Awake()
-    {
-        //yilanin segmentlerini listede tutuldu
-        _segments = new List<Transform>();
-        _segments.Add(transform);
-    }
+
+    private List<Transform> _segments = new();
+    private Vector2Int input;
+    private float nextUpdate;
 
     private void Start()
     {
-        ResetMove();
-    }
-
-    private void ResetMove()
-    {
-        //Default olarak saga hareket edecek
-        _direction = Vector2.right;
-        Time.timeScale = 0.1f;
+        ResetState();
     }
 
     private void Update()
@@ -36,67 +28,153 @@ public class Snake : MonoBehaviour
 
     void GetUserInput()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        // X ekseninde hareket ederken yalnızca yukarı veya aşağı dönmeye izin verir
+        if (_direction.x != 0f)
         {
-            _direction = Vector2.up;
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                input = Vector2Int.up;
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                input = Vector2Int.down;
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.S))
+
+        // Y ekseninde hareket ederken yalnızca sola veya sağa dönmeye izin verir
+        else if (_direction.y != 0f)
         {
-            _direction = Vector2.down;
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            _direction = Vector2.left;
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            _direction = Vector2.right;
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                input = Vector2Int.right;
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                input = Vector2Int.left;
+            }
         }
     }
 
     private void FixedUpdate()
     {
-        SnakeMove();
-        SortingOfSegments();
-    }
+        // Devam etmeden önce bir sonraki güncellemeye kadar beklesin
+        if (Time.time < nextUpdate)
+        {
+            return;
+        }
 
-    private void SnakeMove()
-    {
-        //yilani her zaman bir izgaraya hizalamaliyiz bu nedenle tam sayi degerlerine yuvarladik
-        transform.position = new Vector3(
-            Mathf.Round(transform.position.x) + _direction.x,
-            Mathf.Round(transform.position.y) + _direction.y,
-            0.0f);
-    }
+        // Girişe göre yeni yönü ayarlandı
+        if (input != Vector2Int.zero)
+        {
+            _direction = input;
+        }
 
-    private void Grow()
-    {
-        //yilan segmentleri turetilip listeye eklendi
-        Transform segment = Instantiate(this.segmentPrefab);
+        // Her parçanın konumunu takip ettiği parçayla aynı olacak şekilde ayarlandı.
+        // Konumun önceki konuma ayarlanması için bunu ters sırada yapmalıyız,
+        // aksi takdirde hepsi üst üste istiflenir.
 
-        //uretilen segment nesnesi yilanin sonuna eklenecek
-        //yeni segment listedeki son segmentin pozisyonunda uretilecek
-        segment.position = _segments[_segments.Count - 1].position;
-        _segments.Add(segment);
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        //yiyecek ile snake carpistiginda yiyecek yeni bir segment uretilecek 
-        if (other.tag == "Food")
-            Grow();
-    }
-
-    private void SortingOfSegments()
-    {
-        //Her bir segment onundeki segmenti takip edecek ve basi en son guncellenecek
-        //her segment kuyruktaki bir onceki segmentin pozisyonuna gececek
-        //yilanin son segmentinden baslayarak bir bir geriye dogru ilerler
-        
         for (int i = _segments.Count - 1; i > 0; i--)
         {
             _segments[i].position = _segments[i - 1].position;
         }
 
+        // Yılanı baktığı yöne doğru hareket ettirildi
+        // Izgaraya hizalandığından emin olmak için değerleri yuvarlandı
+        int x = Mathf.RoundToInt(transform.position.x) + _direction.x;
+        int y = Mathf.RoundToInt(transform.position.y) + _direction.y;
+        transform.position = new Vector2(x, y);
+
+        // Hıza göre bir sonraki güncelleme zamanını ayarlandı
+        nextUpdate = Time.time + 1f / (speed * speedMultiplier);
+    }
+    
+
+    private void Grow()
+    {
+        // Yilan segmentleri turetilip listeye eklendi
+        Transform segment = Instantiate(segmentPrefab);
+
+        // Uretilen segment nesnesi yilanin sonuna eklenecek
+        // Yeni segment listedeki son segmentin pozisyonunda uretilecek
+        segment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(segment);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Yiyecek ile snake carpistiginda yiyecek yeni bir segment uretilecek 
+        if (other.gameObject.CompareTag("Food"))
+        {
+            Grow();
+        }
+        else if (other.gameObject.CompareTag("Obstacle"))
+        {
+            ResetState();
+        }
+        else if (other.gameObject.CompareTag("Wall"))
+        {
+            // Yılanın hızı 25f olduğunda duvarlardan geçebilsin.
+            if (moveThroughWalls && speed == 25f )
+            {
+                Traverse(other.transform);
+            }
+            else
+            {
+                ResetState();
+            }
+        }
+    }
+
+    public bool Occupies(int x, int y)
+    {
+        foreach (Transform segment in _segments)
+        {
+            if (Mathf.RoundToInt(segment.position.x) == x &&
+                Mathf.RoundToInt(segment.position.y) == y)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void ResetState()
+    {
+        _direction = Vector2Int.right;
+        transform.position = Vector3.zero;
+
+        //Kafayi yok etmeyi atlamak için 1'den başlatildi
+        for (int i = 1; i < _segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(transform);
+
+        //-1 cunku kafa zaten listede
+        for (int i = 0; i < initialSize - 1; i++)
+        {
+            Grow();
+        }
+    }
+
+    private void Traverse(Transform wall)
+    {
+        //Duvarlardan gecmesi saglandi
+        Vector3 position = transform.position;
+
+        if (_direction.x != 0f)
+        {
+            position.x = Mathf.RoundToInt(-wall.position.x + _direction.x);
+        }
+        else if (_direction.y != 0f)
+        {
+            position.y = Mathf.RoundToInt(-wall.position.y + _direction.y);
+        }
+
+        transform.position = position;
     }
 }
