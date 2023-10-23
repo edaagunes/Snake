43a55bbd280a8eb58ca94ada6545140fb9b27349
edaagunes <edagunes00@hotmail.com>
2023-10-23 18:13:56 +0300

    Created Segments and Grow

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -33,6 +33,5 @@ public class Food : MonoBehaviour
         {
             RandomizePosition();
         }
-        
     }
 }
--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -6,18 +6,26 @@ using UnityEngine.UIElements;
 
 public class Snake : MonoBehaviour
 {
-    
     private Vector2 _direction;
+    private List<Transform> _segments;
+    public Transform segmentPrefab;
+
+    private void Awake()
+    {
+        //yilanin segmentlerini listede tutuldu
+        _segments = new List<Transform>();
+        _segments.Add(transform);
+    }
 
     private void Start()
     {
         ResetMove();
     }
-    
+
     private void ResetMove()
     {
         //Default olarak saga hareket edecek
-        _direction=Vector2.right;
+        _direction = Vector2.right;
         Time.timeScale = 0.1f;
     }
 
@@ -31,28 +39,64 @@ public class Snake : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.W))
         {
             _direction = Vector2.up;
-        }else if (Input.GetKeyDown(KeyCode.S))
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
         {
             _direction = Vector2.down;
-        }else if (Input.GetKeyDown(KeyCode.A))
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
         {
-            _direction=Vector2.left;
-        }else if (Input.GetKeyDown(KeyCode.D))
+            _direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
         {
-            _direction=Vector2.right;
+            _direction = Vector2.right;
         }
     }
+
     private void FixedUpdate()
     {
-        SnakeMove();   
+        SnakeMove();
+        SortingOfSegments();
     }
 
-    void SnakeMove()
+    private void SnakeMove()
     {
         //yilani her zaman bir izgaraya hizalamaliyiz bu nedenle tam sayi degerlerine yuvarladik
         transform.position = new Vector3(
             Mathf.Round(transform.position.x) + _direction.x,
             Mathf.Round(transform.position.y) + _direction.y,
             0.0f);
+    }
+
+    private void Grow()
+    {
+        //yilan segmentleri turetilip listeye eklendi
+        Transform segment = Instantiate(this.segmentPrefab);
+
+        //uretilen segment nesnesi yilanin sonuna eklenecek
+        //yeni segment listedeki son segmentin pozisyonunda uretilecek
+        segment.position = _segments[_segments.Count - 1].position;
+        _segments.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //yiyecek ile snake carpistiginda yiyecek yeni bir segment uretilecek 
+        if (other.tag == "Food")
+            Grow();
+    }
+
+    private void SortingOfSegments()
+    {
+        //Her bir segment onundeki segmenti takip edecek ve basi en son guncellenecek
+        //her segment kuyruktaki bir onceki segmentin pozisyonuna gececek
+        //yilanin son segmentinden baslayarak bir bir geriye dogru ilerler
+        
+        for (int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
     }
 }
